f54f50eaf3f475156924485026f15fc4c2f36aa5
Raí <rai.caje@sou.inteli.edu.br>
2023-07-07 22:17:52 -0300

    Exercício de módulos
    - Criação de módulo/biblioteca personalizado/a

---
+++ Exercícios/C301/ex1.py
@@ -0,0 +1,10 @@
+# EX 1
+# Criação de módulo/biblioteca personalizado/a
+
+def lerString(msgS):
+    print(f"\nA mensagem que você digitou é: {msgS}")
+    return msgS
+
+def lerFloat(msgF):
+    print(f"\nO número que você digitou foi: {msgF}")
+    return msgF
