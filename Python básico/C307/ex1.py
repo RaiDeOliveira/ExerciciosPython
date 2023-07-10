fb74726a86617e8baa9a2e87512989ae9fd1aa2a
Raí <rai.caje@sou.inteli.edu.br>
2023-07-10 06:54:29 -0300

    Reorganização de pastas

--- Python básico/C307/ex1.py
+++ Python básico/C307/ex1.py
@@ -3,12 +3,12 @@
 
 alunos = {'Pedro': 8.0, 'Maria': 10.0, 'Amilton': 7.5}
 
-with open("Exercícios/C307/boletim.txt", 'w') as boletim:
+with open("Python básico/C307/boletim.txt", 'w') as boletim:
     for aluno, nota in alunos.items():
         boletim.write(f'{aluno},{nota}\n')
     boletim.close()
 
-with open("Exercícios/C307/boletim.txt") as boletim:
+with open("Python básico/C307/boletim.txt") as boletim:
     notas = boletim.readlines()
     for linhas in notas:
         print(linhas)
